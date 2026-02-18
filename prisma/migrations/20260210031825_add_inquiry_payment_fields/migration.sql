2f6bd6e15f967d6004db7cb2f485fe5f4426fbdf
Ryder Schilling <ryderscott33@icloud.com>
2026-02-17 22:17:31 -0600

    Init Prisma migrations for thrive DB

--- prisma/migrations/20260210031825_add_inquiry_payment_fields/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Inquiry" ADD COLUMN "paymentAmountCents" INTEGER;
-ALTER TABLE "Inquiry" ADD COLUMN "paymentCurrency" TEXT;
-ALTER TABLE "Inquiry" ADD COLUMN "paymentIntentId" TEXT;
-ALTER TABLE "Inquiry" ADD COLUMN "paymentStatus" TEXT;
